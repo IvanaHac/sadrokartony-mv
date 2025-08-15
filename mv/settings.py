9b3f59cb355466f97cdd98bc9118b33aebabe710
David Prusa <david@David--MacBook-Pro.local>
2025-08-15 21:24:11 +0200

    Zkouška

--- mv/settings.py
+++ mv/settings.py
@@ -136,13 +136,11 @@ TAILWIND_APP_NAME = 'theme'
 
 import os
 from pathlib import Path
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "mv/static") ]   # zdroj statik – u tebe mv/static
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")          # sem se to sesbírá pro produkci
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "mv/static"),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
