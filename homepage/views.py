a4fb361b439d3f429492d6dc3b6a7b7be8c0ae0f
David Prusa <david@Mac.localdomain>
2025-08-31 18:56:03 +0200

    Contact form

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.conf import settings
+
 
 # Create your views here.
 
 def homepageView(req):
+
+    if req.method == "POST":
+
+        phone = req.POST['phone']
+        sender = req.POST['email']
+
+        print('Telefon je:'+ phone)
+        print ('Email je: ' + sender)
+
+        message = f"{req.POST['name']} má zájem o tvé služby. \n\n Telefon na něj je {req.POST['phone']} \n\n Lokalita: {req.POST['place']} \n\n Má zájem o: {req.POST['desc']}." 
+
+        EmailMessage(
+        subject=f"Kontakt z webu – {req.POST['name']}",
+        body=f"Jméno: {req.POST['name']}\nE-mail: {req.POST['email']}\n\nZpráva:\n{message}",
+        from_email=settings.DEFAULT_FROM_EMAIL,   # musí být tvoje @seznam.cz
+        to=[settings.DEFAULT_FROM_EMAIL],
+        reply_to=["bushman@seznam.cz",],
+        ).send()
+
+    
+        # return redirect("success")  # přesměrování po odeslání
 
     content={}
     return render (req, 'homepage/homepage.html', content)
--- mv/settings.py
+++ mv/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k)jns%_3dabdk7eko444g*y3f33kb53+k%oobv@&!*9*!p+@fm'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -147,3 +148,13 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+#E-MAIL
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.seznam.cz"
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = "info@sadrokartony-mv.cz"
+EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD") # bez MFA = běžné heslo; s MFA = aplikační heslo
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
